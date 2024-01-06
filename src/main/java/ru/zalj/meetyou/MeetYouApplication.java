0d53fe1451a7bb2df2e5896179011b9516572963
ZaljORIGINAL <zalj.kod@gmail.com>
2024-01-06 12:51:40 +0300

    Init repo

---
+++ src/main/java/ru/zalj/meetyou/MeetYouApplication.java
@@ -0,0 +1,13 @@
+package ru.zalj.meetyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetYouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetYouApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/zalj/meetyou/MeetYouApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.zalj.meetyou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetYouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
